9825520c4d8d25a05e4b0c45c127a78d09b2f6dc
Skylar Cupit <Skylarcupit@gmail.com>
2023-02-12 23:00:23 -0600

    Generate an additional IOS-friendly PDF when packaging (all box shadows removed)

--- make-pdf.js
+++ make-pdf.js
@@ -1,9 +1,54 @@
 const puppeteer = require("puppeteer");
 
-async function makePDFFromPage(pageURL, outFilePath) {
+const urls = {
+  dev: "http://localhost:5173",
+  localDist: `file://${__dirname}/dist/index.html`,
+  preview: "http://localhost:4173",
+};
+
+withBrowserDo(browser => {
+  return Promise.all([
+    makePDFFromPage(
+      browser,
+      urls.localDist,
+      "./dist/Skylar_Cupit_Resume.pdf",
+      { omitShadows: false }
+    ),
+    makePDFFromPage(
+      browser,
+      urls.localDist,
+      "./dist/Skylar_Cupit_Resume_IOS_Friendly.pdf",
+      { omitShadows: true }
+    )
+  ])
+})
+.catch(console.error);
+
+// ========================================
+// Helper functions 
+// ========================================
+
+async function withBrowserDo(actionFunc) {
   const browser = await puppeteer.launch({headless: true});
+  await actionFunc(browser);
+  return await browser.close();
+}
+
+async function makePDFFromPage(
+  browser,
+  pageURL,
+  outFilePath,
+  options
+) {
   const page = await browser.newPage();
+
   await page.goto(pageURL, {waitUntil: "domcontentloaded"});
+
+  if (options.omitShadows) {
+    await page.addStyleTag({
+      content: "* { box-shadow: none !important; }"
+    });
+  }
 
   await page.pdf({
     preferCSSPageSize: true,
@@ -16,17 +61,4 @@ async function makePDFFromPage(pageURL, outFilePath) {
       right: 0
     }
   });
-  await browser.close();
 }
-
-const urls = {
-  dev: "http://localhost:5173",
-  localDist: `file://${__dirname}/dist/index.html`,
-  preview: "http://localhost:4173",
-};
-
-makePDFFromPage(
-  urls.localDist,
-  "./dist/Skylar_Cupit_Resume.pdf"
-)
-  .catch(console.error)
